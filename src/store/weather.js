b1da5a28d4eb3b3a916329dd5db270c8f93e9f82
Fadhli Syaifullah <66958437+fadhlisy@users.noreply.github.com>
2020-11-11 16:05:25 +0700

    Fix: empty city

--- src/store/weather.js
+++ src/store/weather.js
@@ -2,8 +2,10 @@ import { KEY, API_WEATHER, API_FORECAST } from '../constans';
 
 function state() {
     return {
-        city: 'Jakarta',
-        unit: 'celcius',
+        default: {
+            city: 'Jakarta',
+            unit: 'celcius'
+        },
         data: null,
         recently: null,
         error: null
@@ -19,14 +21,12 @@ const mutations = {
     }
 }
 const actions = {
-    // Get weather data
-    getWeather: async function({ state, commit, dispatch }) {
+    getWeather: async function({ state, commit, dispatch }, city) {
         state.data = null;
         state.recently = null;
         state.error = null;
-        // Make delay to show loading display
         setTimeout(async function() {
-            var req = await fetch(`${ API_WEATHER }?q=${ state.city }&appid=${ KEY }`);
+            var req = await fetch(`${ API_WEATHER }?q=${ city ? city : state.default.city }&appid=${ KEY }`);
             var res = await req.json();
             if (res.cod === 200) {
                 var recent = await dispatch('getRecently');
@@ -37,9 +37,8 @@ const actions = {
             }
         }, 1000)
     },
-    // Get forecast data
-    getRecently: async function({ state }) {
-        var req = await fetch(`${ API_FORECAST }?q=${ state.city }&appid=${ KEY }`);
+    getRecently: async function({ state }, city) {
+        var req = await fetch(`${ API_FORECAST }?q=${ city ? city : state.default.city }&appid=${ KEY }`);
         var res = await req.json();
         return res.list.splice(0, 8);
     }
