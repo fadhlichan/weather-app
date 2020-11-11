c9a3eaad1fa632a04c59804e66633c5410c08fb0
Fadhli Syaifullah <66958437+fadhlisy@users.noreply.github.com>
2020-11-11 16:43:03 +0700

    Update weather.js

--- src/store/weather.js
+++ src/store/weather.js
@@ -1,4 +1,5 @@
-import { KEY, API_WEATHER, API_FORECAST } from '../constans';
+import _ from 'lodash'
+import { KEY, API_WEATHER, API_FORECAST, COUNTRIES } from '../constans'
 
 function state() {
     return {
@@ -14,6 +15,7 @@ function state() {
 
 const mutations = {
     setData: function(state, data) {
+        data.sys.country = _.find(COUNTRIES, { code: data.sys.country }).name;
         state.data = data;
     },
     setRecently: function(state, recent) {
